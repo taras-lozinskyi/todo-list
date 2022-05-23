5d5cdbd125f2774836b790a49d07146b1d182fae
Taras <tlozinskiy62@gmail.com>
2022-05-23 20:05:29 +0300

    зробив видалення всіх виконаних елементів а також покзувати кнопку яка видаляє всі виконані елементи тільки тоді коли вони є

--- src/componets/Filter/Filter.jsx
+++ src/componets/Filter/Filter.jsx
@@ -1,45 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const Filter = ({ taskList,  setMyFilter, myFilter}) => {
-  
-  // const clearCompleted = () =>{
-  //   let newList = [...myFilter].filter(item=> item.status !== true )
-  //   setMyFilter(newList)
-  // }
+export const Filter = ({ taskList, setTaskList, setMyFilter, myFilter }) => {
+  const clearCompleted = () => {
+    let newList = [...taskList].filter((item) => item.status !== true);
+    setTaskList(newList);
+  };
 
+  const visibleButton = () => {
+    return [...taskList].some((item) => item.status === true);
+  };
+  console.log(visibleButton())
 
-    const todoFilter =(status) =>{
-        if(status === 'all'){
-            setMyFilter(taskList)
-        }
-        else{
-            let newList = [...taskList].filter(item => item.status === status)
-            setMyFilter(newList)
-        }
-            
+   useEffect(()=>{
+     visibleButton()
+   },[taskList])
+
+  const todoFilter = (status) => {
+    if (status === "all") {
+      setMyFilter(taskList);
+    } else {
+      let newList = [...taskList].filter((item) => item.status === status);
+      setMyFilter(newList);
     }
+  };
 
   return (
     <div>
-      <div className="flex justify-between px-4 h-10 items-center text-gray-400 ">
-        <div>{[...taskList].filter(item=> item.status===false).length} items left</div>
-        <button  onClick={()=> todoFilter('all')}className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-10">
-          All
-        </button>
-        <button
+      <div className="flex justify-between pl-4  pr-4 h-10 items-center text-gray-400 ">
+        <div className="w-[330px] flex justify-between">
+        <div className="flex items-center">
+          {[...taskList].filter((item) => item.status === false).length} items
+          left
+        </div>
         
-          onClick={() => todoFilter(false)}
-          className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-14"
-        >
-          Action
-        </button>
-        <button
-          onClick={() => todoFilter(true)}
-          className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-24"
-        >
-          Completed
-        </button>
-        <button className="hover:underline">Clear completed</button>
+          <button
+            onClick={() => todoFilter("all")}
+            className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-10"
+          >
+            All
+          </button>
+          <button
+            onClick={() => todoFilter(false)}
+            className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-14"
+          >
+            Action
+          </button>
+          <button
+            onClick={() => todoFilter(true)}
+            className="focus:border hover:border hover:border-[rgba(175,47,47,0.2)] focus:border-[rgba(175,47,47,0.2)] w-24"
+          >
+            Completed
+          </button>
+        </div>
+     
+        {visibleButton()&& (
+          <button onClick={() => clearCompleted()} className="hover:underline ">
+            Clear completed
+          </button>
+        )}
       </div>
     </div>
   );
